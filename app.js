20edeb960b3f531bce70aa633f74bcc0871a083f
정준용 <59636584+JUNGJUNYONG@users.noreply.github.com>
2021-05-15 01:55:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 import router from './src/router/main.js';
 import ejs from'ejs'
 new router(app);
-let port = process.env.port || 8081;
+let port = process.env.port || 8082;
 
 app.use(express.static('public'));
 app.use(express.static('./src/views'));
 app.set('views','./src/views');
 app.engine('html', ejs.renderFile);
 
-const server = app.listen(8081,function(){
+const server = app.listen(8082,function(){
     console.log(port,'open');
 })
